79729d5ddefaf2de74c6df0d5bda8a9d4208c30b
Sargaroth2511 <84723008+Sargaroth2511@users.noreply.github.com>
2022-05-07 16:11:38 +0200

    updated gitignore

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom/client';
 import './styles.scss';
 import App from './App';
 
+
+console.log(process.env.REACT_APP_ACCESS_TOKEN)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
