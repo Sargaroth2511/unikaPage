c14dcdcc439fdbaffb5c4a2fd55381cb16152fa0
Sargaroth2511 <84723008+Sargaroth2511@users.noreply.github.com>
2022-05-07 18:36:51 +0200

    test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './styles.scss';
 import App from './App';
 
 
-console.log(process.env.REACT_APP_ACCESS_TOKEN)
+console.log(process.env.REACT_APP_SPACE_ID)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
